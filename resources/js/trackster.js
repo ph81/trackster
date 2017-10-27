7691d6d58c2311019925231ae9ff0442cfc0e63d
EM <paperhearts81@gmail.com>
2017-10-27 18:28:13 -0500

    Sorting option added

--- resources/js/trackster.js
+++ resources/js/trackster.js
@@ -3,13 +3,107 @@ var Trackster = {};
 
 var API_KEY = 'ea3f97421d04a8631a35d75c3e7f9838';
 
+var query = 'none';
+
+var sortValue = 0;
+var sortOption = 1;
+
 
 $(document).ready(function() {
   $('#search-btn').click(function (event) {
+
+    // cancelling default event
     event.preventDefault();
+
     Trackster.searchTracksByTitle($('#search-field').val());
+
+    //sorting by field
+
+    $(".sort").click(function() {
+
+      switch (sortValue) {
+      case "none":
+        break;
+      case "track":
+        switch ($(this).text()) {
+          case "Song ":
+            Trackster.columnSort("name");
+            break;
+          case "Artist ":
+            Trackster.columnSort("artist");
+            break;
+            /*
+          case "Art. ":
+            Trackster.columnSort("artist");
+            break;
+            */
+          case "Listeners ":
+            Trackster.columnSort("listeners");
+            break;
+            /*
+          case "Pop. ":
+            Trackster.columnSort("listeners");
+            break;
+            */
+        }
+        break;
+      case "artist":
+        switch ($(this).text()) {
+          case "Song ":
+            break;
+          case "Artist ":
+            Trackster.columnSort("name");
+            break;
+          case "Art. ":
+            Trackster.columnSort("name");
+            break;
+          case "Popularity ":
+            Trackster.columnSort("listeners");
+            break;
+          case "Pop. ":
+            Trackster.columnSort("listeners");
+            break;
+        }
+        break;
+      }
+    });
+
   });
 });
+
+/*
+  Given a search term as a string, query the LastFM API.
+  Render the tracks given in the API query response.
+*/
+
+Trackster.searchTracksByTitle = function(title) {
+
+  // logo animated
+  $('.title').addClass("animate");
+
+  //assembling URL
+
+  var api_url = 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json';
+  console.log(api_url);
+
+  $.ajax({
+    dataType: "json",
+    url: api_url,
+        success: function(response) {
+          console.log('PASS');
+          //Trackster.renderTracks(response.results.trackmatches.track);
+          Tracks = response.results.trackmatches.track;
+          Trackster.renderTracks(Tracks);
+          sortValue = 'track';
+          //logo goes back to normal searchTracksByTitle
+          $('.title').removeClass("animate");
+
+        },
+        error: function(response) {
+          console.log(response);
+        }
+  });
+};
 
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
@@ -37,7 +131,7 @@ Trackster.renderTracks = function(tracks) {
     '    </a>' +
     '  </div>' +
     '  <div class="col-xs-4">' + track.name + '</div>' +
-    '  <div class="col-xs-2">' + track.artist + '</div>' +
+    '  <div class="col-xs-2 artist">' + track.artist + '</div>' +
     '  <div class="col-xs-2"><img src="' + coverAlbum + '"/></div>' +
     '  <div class="col-xs-2">' + track.listeners + '</div>' +
     '</div>';
@@ -48,27 +142,65 @@ Trackster.renderTracks = function(tracks) {
 
 };
 
-/*
-  Given a search term as a string, query the LastFM API.
-  Render the tracks given in the API query response.
-*/
+/* Sorting fields */
 
-Trackster.searchTracksByTitle = function(title) {
-
-  //assembling URL
-
-  var api_url = 'https://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key=' + API_KEY + '&format=json';
-  console.log(api_url);
-
-  $.ajax({
-    dataType: "json",
-    url: api_url,
-        success: function(response) {
-          console.log('PASS');
-          Trackster.renderTracks(response.results.trackmatches.track);
-        },
-        error: function(response) {
-          console.log(response);
+Trackster.columnSort = function(columnID) {
+  switch (sortValue) {
+    case "track":
+      if (columnID === "listeners") {
+        if (sortOption === 1) {
+          Tracks.sort(function(a, b) {
+            sortOption = 0;
+            return b[columnID] - a[columnID];
+          });
+        } else {
+          Tracks.sort(function(a, b) {
+            sortOption = 1;
+            return a[columnID] - b[columnID];
+          });
         }
-  });
+      } else {
+        if (sortOption === 1) {
+          Tracks.sort(function(a, b) {
+            sortOption = 0;
+            return (a[columnID]).localeCompare(b[columnID]);
+          });
+        } else {
+          Tracks.sort(function(a, b) {
+            sortOption = 1;
+            return (b[columnID]).localeCompare(a[columnID]);
+          });
+        }
+      }
+      Trackster.renderTracks(Tracks);
+      break;
+    case "artist":
+      if (columnID === "listeners") {
+        if (sortOption === 1) {
+          Artists.sort(function(a, b) {
+            sortOption = 0;
+            return b[columnID] - a[columnID];
+          });
+        } else {
+          Artists.sort(function(a, b) {
+            sortOption = 1;
+            return a[columnID] - b[columnID];
+          });
+        }
+      } else {
+        if (sortOption === 1) {
+          Artists.sort(function(a, b) {
+            sortOption = 0;
+            return (a[columnID]).localeCompare(b[columnID]);
+          });
+        } else {
+          Artists.sort(function(a, b) {
+            sortOption = 1;
+            return (b[columnID]).localeCompare(a[columnID]);
+          });
+        }
+      }
+      //Trackster.renderArtists(Artists);
+      break;
+  }
 };
